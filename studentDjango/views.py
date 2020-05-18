c8bec6b9880dc402ea86dba657bd32b43a066ccc
cuijianzhe <598941324@qq.com>
2020-05-18 18:29:08 +0800

    20200518

---
+++ studentDjango/views.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# @Time  : 2020/5/18 17:42
+# @Author : cuijianzhe
+# @File  : views.py
+# @Software: PyCharm
+from django.shortcuts import HttpResponse, render, redirect
+def yimi(request):
+    #request参数保存了所有和用户浏览器相关请求的数据
+    # with open("templates/yimi.html","r",encoding="utf-8") as f:
+    #     data = f.read()
+    # return HttpResponse(data)
+    return render(request,"yimi.html")
+
+def xiaohei(request):
+    #request参数保存了所有和用户浏览器相关请求的数据
+    return render(request,'word-clock.html')
+
+def APuser(request):
+    return render(request,"AP_user.html")
+def LiveRoom_in(request):
+    return render(request,"Liveroom_in.html")
+
+def login(request):
+    error_msg = ""
+    if request.method == 'GET':  #这里必须是大写
+        return render(request,'login.html')
+    else:
+        #如果你是POST请求，我就取出提交的数据，做登录判断
+        email = request.POST.get("email",None)
+        pwd = request.POST.get('pwd',None)
+        print(email,pwd)
+
+    #做是否登陆成功的判断
+    if email == "598941324@qq.com" and pwd == "598941324":
+        return redirect("https://www.cjzshilong.cn")
+    else:
+        error_msg = "邮箱或者密码错误"
+    # 不是POST请求就走下面这一句
+    return render(request,"login.html",{"error": error_msg})
+
+
